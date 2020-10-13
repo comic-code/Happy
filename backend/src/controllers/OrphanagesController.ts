7239db2606cf94ca7eb2c82e2eca9066b94a548d
Cômico <bitotajima@hotmail.com>
2020-10-13 20:44:30 -0300

    Dia 2 - Listagem de Orfanatos :white_check_mark:

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,8 +1,28 @@
-Dimport { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm'; 
+
 import Orphanages from '../models/Orphanage';
 
 export default {
+
+  async index(request: Request, response: Response) {
+    const orphanagesRepository = getRepository(Orphanages);
+
+    const orphanages = await orphanagesRepository.find();
+
+    return response.json(orphanages);
+  },
+
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const orphanagesRepository = getRepository(Orphanages);
+
+    const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+    return response.json(orphanage);
+  },
+
   async create(request: Request, response: Response) {
     const {
       name, 
