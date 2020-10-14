454650b4d6be3350a2330ae9ced8a9143d96ea90
Cômico <bitotajima@hotmail.com>
2020-10-14 13:24:05 -0300

    Dia 2 - Perfil do Orfanato ✅

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm'; 
+import orphanageView from '../views/orphanages_view';
 
 import Orphanages from '../models/Orphanage';
 
@@ -8,9 +9,11 @@ export default {
   async index(request: Request, response: Response) {
     const orphanagesRepository = getRepository(Orphanages);
 
-    const orphanages = await orphanagesRepository.find();
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images']
+    });
 
-    return response.json(orphanages);
+    return response.json(orphanageView.renderMany(orphanages));
   },
 
   async show(request: Request, response: Response) {
@@ -18,9 +21,11 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanages);
 
-    const orphanage = await orphanagesRepository.findOneOrFail(id);
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images']
+    });
 
-    return response.json(orphanage);
+    return response.json(orphanageView.render(orphanage));
   },
 
   async create(request: Request, response: Response) {
@@ -36,6 +41,12 @@ export default {
   
     const orphanagesRepository = getRepository(Orphanages);
   
+    const requestImages = request.files as Express.Multer.File[];
+
+    const images = requestImages.map(image => {
+      return { path: image.filename }
+    })
+
     const orphanage = orphanagesRepository.create({
       name, 
       latitude,
@@ -43,7 +54,8 @@ export default {
       about,
       instructions,
       opening_hours,
-      open_on_weekends
+      open_on_weekends,
+      images
     });
   
     await orphanagesRepository.save(orphanage);
